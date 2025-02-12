d21f4d20a4e62a8714cc1d3afef9f9cfeef075b3
phu-phurithat <phurithat.komkham@gmail.com>
2025-02-13 00:50:58 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,4 @@
-const dotenv = require("dotenv");
-
-const API_KEY = process.env.API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
